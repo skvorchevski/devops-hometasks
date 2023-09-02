ece80dedd708376b33d912b1e9d7f7cd4fc68914
Skvorchevskiy A <askvorchevskiy@gmail.com>
2023-09-02 12:05:59 +0300

    Managed by Terraform

--- 13-terraform/github-repo.tf
+++ 13-terraform/github-repo.tf
@@ -1,8 +1,10 @@
-resource "github_repository_file" "terraform-state" {
-  repository          = var.github_repository_url
+resource "github_repository_file" "file" {
+  for_each = fileset("${path.module}", "*.{tf,tfvars}")
+
+  repository          = var.github_repository
   branch              = "feature/13-terraform"
-  file                = "13-terraform/terraform.tfstate"
-  content             = "**/*.tfstate"
+  file                = "13-terraform/${each.value}"
+  content             = file("${path.module}/${each.value}")
   commit_message      = "Managed by Terraform"
   commit_author       = "Skvorchevskiy A"
   commit_email        = "askvorchevskiy@gmail.com"
