d8efa88fc8ada903ef899f18cc002ed419d71e86
Skvorchevskiy A <askvorchevskiy@gmail.com>
2023-09-02 12:06:00 +0300

    Managed by Terraform

--- 13-terraform/service.tf
+++ 13-terraform/service.tf
@@ -1,8 +1,6 @@
 resource "kubernetes_service" "wcg-service" {
-  count = var.create_index ? 1 : 0
   metadata {
     name      = "wcg-service"
-    namespace = kubernetes_namespace.wsg-ns[count.index].metadata[0].name
   }
   spec {
     selector = {
