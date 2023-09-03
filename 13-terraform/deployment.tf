04e00d5bc7694e07b6180694f59df45f8b0bae30
Skvorchevskiy A <askvorchevskiy@gmail.com>
2023-09-03 12:39:01 +0300

    Managed by Terraform

---
+++ 13-terraform/deployment.tf
@@ -0,0 +1,36 @@
+resource "kubernetes_deployment" "wcg-deployment-resource" {
+  count = var.create_index ? 1 : 0
+
+  metadata {
+    name = var.deploy_name
+    namespace = kubernetes_namespace.wsg-ns[count.index].metadata[0].name
+  }
+
+  spec {
+    replicas = var.replicaCount
+
+    selector {
+      match_labels = {
+        app = var.app_label
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = var.app_label
+        }
+      }
+
+      spec {
+        container {
+          image = "${var.image_repository}:${var.image_tag}"
+          name  = var.image_name
+          port {
+            container_port = var.container_port
+          }
+        }
+      }
+    }
+  }
+}
